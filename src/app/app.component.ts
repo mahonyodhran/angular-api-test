5fea82d5727c06e0deefacc4922b8b12d3029ce0
Odhran <mahonyodhran@gmail.com>
2022-07-09 18:03:43 +0100

    updated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-api-test';
+  constructor(private http: HttpClient) {}
 }
