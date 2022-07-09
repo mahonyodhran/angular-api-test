70904971651362a712c6ee86b255151f124bd5f1
Odhran <mahonyodhran@gmail.com>
2022-07-09 23:29:41 +0100

    added POST on load for post-item, check console for success 304

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,22 @@ import { AppComponent } from './app.component';
 import { GetTodosComponent } from './get-todos/get-todos.component';
 import { LandingComponent } from './landing/landing.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostItemComponent } from './post-item/post-item.component';
 
 const appRoutes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'get-todos', component: GetTodosComponent },
+  { path: 'post-item', component: PostItemComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, GetTodosComponent, LandingComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    GetTodosComponent,
+    LandingComponent,
+    NavbarComponent,
+    PostItemComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
---
+++ src/app/post-item/post-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css'],
+})
+export class PostItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+}
