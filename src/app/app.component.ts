78e6f0d2ea66d67cbff4337544fe2b6e546cf7e5
Odhran <mahonyodhran@gmail.com>
2022-07-09 20:34:25 +0100

    updated to getTodos from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title = 'Angular API Test';
+
   constructor(private http: HttpClient) {}
+
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+
+  todos: Observable<any>;
+
+  getTodos() {
+    let params = new HttpParams().set('userId', '1');
+    let headers = new HttpHeaders().set('Authorization', 'auth-token');
+
+    this.todos = this.http.get(this.ROOT_URL + '/todos', { params, headers });
+  }
+
+  ngOnInit(): void {}
 }
